b499b9daa15ac50d0c1f31026d8be609262dac34
Carlos Eduardo <silvacarlosoliveira@outlook.com>
2021-08-27 14:33:31 -0300

    inicialização do curso

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./estilo.css";
+class FormularioCadastro extends Component {
+
+  constructor() {
+    super();
+    this.titulo = "";
+  }
+
+  handleMudancaTitulo(event) {
+    this.titulo = event.target.value;
+  }
+
+  render() {
+    return (
+      <form className="form-cadastro ">
+        <input
+          type="text"
+          placeholder="Título"
+          className="form-cadastro_input"
+          onChange={this.handleMudancaTitulo}
+        />
+        <textarea
+          rows={15}
+          placeholder="Escreva sua nota..."
+          className="form-cadastro_input"
+        />
+        <button className="form-cadastro_input form-cadastro_submit">
+          Criar Nota
+        </button>
+      </form>
+    );
+  }
+}
+
+export default FormularioCadastro;
